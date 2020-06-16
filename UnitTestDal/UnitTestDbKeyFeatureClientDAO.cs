8bc070ab5ca4ac91b116a0709850480292eddcab
AborvalovMV <AborvalovMV@GEOFT-ABORVALOV>
2020-06-16 14:05:31 +0400

    test keyFeature add.

--- UnitTestDal/UnitTestDbKeyFeatureClientDAO.cs
+++ UnitTestDal/UnitTestDbKeyFeatureClientDAO.cs
@@ -11,26 +11,26 @@ namespace UnitTestDal
     {
         private IContractKeyFeatureClientDAO keyFeatureClientDAO;
 
-        //[TestMethod]
-        //public void AddTest()
-        //{
-        //    int idExpected = 1;
-        //    int add;
-        //    KeyFeatureClient kfc = new KeyFeatureClient
-        //    {
-        //        IdClient     = 1,
-        //        IdKeyFeature = 1,
-        //        Initiator    = "Test Testovich",
-        //        Note         = "Bla bla bla.",
-        //    };
+        [TestMethod]
+        public void AddTest()
+        {
+            int idExpected = 1;
+            int add;
+            KeyFeatureClient kfc = new KeyFeatureClient
+            {
+                IdClient = 1,
+                IdKeyFeature = 1,
+                Initiator = "Test Testovich",
+                Note = "Bla bla bla.",
+            };
 
-        //    using (var db = new EntitesContext())
-        //    {
-        //        keyFeatureClientDAO = new DbKeyFaetureClientDAO(db);
-        //        add = keyFeatureClientDAO.Add(kfc);
-        //    }
+            using (var db = new EntitesContext())
+            {
+                keyFeatureClientDAO = new DbKeyFaetureClientDAO(db);
+                add = keyFeatureClientDAO.Add(kfc);
+            }
 
-        //    Assert.AreEqual(idExpected, add);
-        //}  
+            Assert.AreEqual(idExpected, add);
+        }
     }
 }
--- UnitTestDal/UnitTestDbKeyFeatureDAO.cs
+++ UnitTestDal/UnitTestDbKeyFeatureDAO.cs
@@ -12,17 +12,77 @@ namespace UnitTestDal
     public class UnitTestDbKeyFeatureDAO
     {
         private IContractKeyFeatureDAO kfDAO;
+        private DateTime date = DateTime.Now.Date;
+
         [TestMethod]
         public void NullEntitesContextKeyFeature()
         {
             Assert.ThrowsException<ArgumentNullException>(() => kfDAO = new DbKeyFeatureDAO(null));
         }
         [TestMethod]
-        public void TestMethod1()
+        public void AddKeyFeature()
         {
-            //
-            // TODO: Add test logic here
-            //
+            int idExpected = 1;
+            int add;
+
+            using (var db = new EntitesContext())
+            {
+                kfDAO = new DbKeyFeatureDAO(db);
+                add = kfDAO.Add(CreateNew());
+                ClearTable.KeyFeatures(db);
+            }
+
+            Assert.AreEqual(add, idExpected);
+        }
+        [TestMethod]
+        public void AddNullKeyFeature()
+        {
+            using (var db = new EntitesContext())
+            {
+                kfDAO = new DbKeyFeatureDAO(db);
+                Assert.ThrowsException<ArgumentNullException>(() => kfDAO.Add(null));
+            }
+        }
+        [TestMethod]
+        public void AddDuplicateKeyFeature()
+        {
+            using (var db = new EntitesContext())
+            {
+                kfDAO = new DbKeyFeatureDAO(db);
+                kfDAO.Add(CreateNew());
+                Assert.ThrowsException<Exception>(() => kfDAO.Add(CreateNew()));
+                ClearTable.KeyFeatures(db);
+            }
+        }
+
+
+
+
+
+
+
+        private KeyFeature CreateNew()
+        {
+            return new KeyFeature
+            {
+                IdFeature = 1,
+                IdHaspKey = 1,
+                StartDate = date,
+                EndDate = date.AddDays(14),
+            };
+        }
+        private KeyFeature CreateNew(int id)
+        {
+            KeyFeature kf = CreateNew();
+            kf.Id = id;
+            return kf;
+        }
+        private KeyFeature CreateNew(int id, int idHaspKey, int idFeature)
+        {
+            KeyFeature kf = CreateNew(id);
+            kf.IdHaspKey = idHaspKey;
+            kf.IdFeature = idFeature;
+            return kf;
         }
     }
 }
