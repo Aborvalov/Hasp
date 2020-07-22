180add6d615d842aa5f9ce345001a155f9d740da
AborvalovMV <AborvalovMV@GEOFT-ABORVALOV>
2020-07-22 11:48:48 +0400

    Распеределение файлов по папеам. Заппись выбранной базы в файл.

--- Model/SelectedDataBaseModel.cs
+++ Model/SelectedDataBaseModel.cs
@@ -1,20 +1,38 @@
 ﻿using ModelEntities;
+using System.IO;
 
 namespace Model
 {
     public class SelectedDataBaseModel : ISelectedDataBaseModel
     {
-        static TypeDataBase typeDateBase = (TypeDataBase)4;
+        private const string path = @".\DataBase.txt";
 
         public void EditItem(TypeDataBase dateBase)
         {
-            typeDateBase = dateBase;
+            using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+            {
+                sw.Write((int)dateBase);
+            }
         }
 
         public TypeDataBase GetItem()
         {
-            return typeDateBase;
-        }
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    return (TypeDataBase)int.Parse(sr.ReadToEnd());
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+                {
+                    sw.Write(-1);
+                }
+            }
 
+            return (TypeDataBase)(-1);
+        }
     }
 }
