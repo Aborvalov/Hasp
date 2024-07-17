795508fbc2d8632762b965e41d6c5913e5fc9e97
Maria Loskutova <110923631+minstradamuss@users.noreply.github.com>
2024-07-17 11:21:39 +0400

    исправления

--- Model/Context/Context.cs
+++ Model/Context/Context.cs
@@ -5,12 +5,11 @@ namespace Model
 {
     internal static class Context
     {
-        private static IEntitesContext context = new EntitesContext();
-
+       
 
         internal static IEntitesContext GetContext()
         {
-            return context;
+            return new EntitesContext();
         }        
     }
 }
