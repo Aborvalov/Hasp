467c4b95b863bad1d0103782aac04cf7b5ac0145
Maria Loskutova <110923631+minstradamuss@users.noreply.github.com>
2024-07-17 09:46:22 +0400

    исправления

--- Model/Context/Context.cs
+++ Model/Context/Context.cs
@@ -5,15 +5,9 @@ namespace Model
 {
     internal static class Context
     {
-        private static IFactoryContext FactoryContext { get; }
-        static Context()
-        {
-            FactoryContext = new FactoryContext();
-        }
-
         internal static IEntitesContext GetContext()
         {
-            return FactoryContext.CreateWorkContext();
+            return new EntitesContext();
         }        
     }
 }
